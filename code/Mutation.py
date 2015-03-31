fcc6594b3987f39520a181e920d50ab1d8819698
Sander van Rijn <svr003@gmail.com>
2015-03-31 14:35:38 +0200

    Added new small functions for (1+1)-ES

--- code/Mutation.py
+++ code/Mutation.py
@@ -27,3 +27,32 @@ def x1(x, sigma, z):
     return new_x
 
 
+def adaptSigma(sigma, p_s, c=0.817):
+    """
+    Adapt parameter sigma based on the 1/5th success rule
+
+    :param sigma:
+    :param p_s:
+    :param c:
+    :return:
+    """
+
+    if p_s < 1/5:
+        sigma *= c
+    elif p_s > 1/5:
+        sigma /= c
+
+    return sigma
+
+
+def calculateP_S(num_successes, num_failures):
+    """
+    Calculate the recent success rate
+
+    :param num_successes:
+    :param num_failures:
+    :return:
+    """
+
+    return num_successes / (num_failures + num_successes)
+
