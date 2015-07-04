f3a924a45e7900652ce8cb38fcf1c10d0616b3cd
Sander van Rijn <svr003@gmail.com>
2015-07-04 17:22:58 +0200

    Added average & weighted average recombination

--- code/Recombination.py
+++ code/Recombination.py
@@ -10,17 +10,32 @@ that are to form the new population
 import numpy as np
 
 
-def average(mu, population):
+def average(labda, population):
     """
-        Given the population, return the average of the mu best individuals
+        Given the new population, return the average of the mu best individuals
     """
 
-    pass
+    avg = population[0].getCopy()
+    avg.dna = np.mean([individual.dna for individual in population])
+
+    new_population = [avg]
+    for _ in range(labda-1):
+        new_population.append(avg.getCopy())
+
+    return new_population
 
 
-def weighted(mu, weights, population):
+def weighted(labda, weights, population):
     """
         Given the population and weights, return the weighted average of the mu best
     """
 
-    pass
+    mu = len(population)
+    avg = population[0].getCopy()
+    avg.dna = np.mean([population[i].dna * weights[i] for i in range(mu)])
+
+    new_population = [avg]
+    for _ in range(labda-1):
+        new_population.append(avg.getCopy())
+
+    return new_population
