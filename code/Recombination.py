03fd075bdcbb52b5c67ad73fa5456766763c6d59
Sander van Rijn <svr003@gmail.com>
2015-08-21 11:35:47 +0200

    Massive problem solved: was summing all matrices into a single number rather than a single matrix

--- code/Recombination.py
+++ code/Recombination.py
@@ -40,7 +40,7 @@ def weighted(population, parameters):
     mu = len(population)
     avg = population[0].getCopy()
     parameters.y_w = np.sum([population[i].last_s * parameters.weights[i] for i in range(mu)], axis=0)
-    parameters.y_w_squared = np.sum([np.dot(population[i].last_s, population[i].last_s.T) * parameters.weights[i] for i in range(mu)])
+    parameters.y_w_squared = np.sum([np.dot(population[i].last_s, population[i].last_s.T) * parameters.weights[i] for i in range(mu)], axis=0)
 
     mean = np.mean([population[i].dna for i in range(mu)], axis=0)
     avg.dna = mean + parameters.y_w
