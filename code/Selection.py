b01627a17fc3e9976e0348cd574e1ff2199c71b0
Sander van Rijn <svr003@gmail.com>
2015-10-08 15:51:15 +0200

    Added pairwise selection function

--- code/Selection.py
+++ code/Selection.py
@@ -14,12 +14,7 @@ that are chosen to be the best of this generation
 
 
 def best(population, new_population, parameters):
-    """
-        Given the population, return the (mu) best
-    """
-    if parameters.mu < 1:
-        raise Exception("best() has to return at least one individual")
-
+    """ Given the population, return the (mu) best """
     if parameters.elitist:
         new_population.extend(population)
 
@@ -28,10 +23,30 @@ def best(population, new_population, parameters):
     return new_population[:parameters.mu]
 
 
+def pairwise(population, new_population, parameters):
+    """
+        Perform a pairwise selection on a population.
+        Intended for use with a mirrored sampling strategy to prevent bias.
+
+        Assumes that new_population contains pairs as [P1_a, P1_b, P2_a, P2_b, etc ... ]
+    """
+    if len(new_population) % 2 != 0:
+        raise Exception("Error: attempting to perform pairwise selection on an odd number of individuals")
+
+    # Select the best (=lowest) fitness for each consecutive pair of individuals
+    pairwise_filtered = []
+    for i in range(len(new_population), step=2):
+        if new_population[i].fitness < new_population[i+1].fitness:
+            pairwise_filtered.append(new_population[i])
+        else:
+            pairwise_filtered.append(new_population[i+1])
+
+    # After pairwise filtering, we can re-use the regular selection function
+    return best(population, pairwise_filtered, parameters)
+
+
 def onePlusOneSelection(population, new_population, t, parameters):
-    """
-        (1+1)-selection (with success history)
-    """
+    """ (1+1)-selection (with success history) """
 
     new_individual = new_population[0]
     individual = population[0]
