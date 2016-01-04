76079ab007f827546a5dd8e9686af4c2608e8bfa
Sander <svr003@gmail.com>
2016-01-04 13:49:21 +0100

    Added config file for hardcoded values

---
+++ code/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__author__ = 'Sander van Rijn <svr003@gmail.com>'
+
+### GA Settings ###
+GA_mu = 3        # Assuming a dimensionality of 11
+GA_lambda = 12   # (8 boolean + 3 triples)
+GA_budget = 250  # Roughly 20 generations
+GA_parallel = True
+
+### ES Settings ###
+ES_budget_factor = 1e3
+ES_num_runs = 15
+ES_parallel = False
+
+### Experiment Settings ###
+experiment_dims = (2, 3, 5, 10, 20)  # Problem dimensionalities to be tested
+experiment_funcs = (3, 4, 7, 9, 10, 12, 13, 16, 17, 19, 20, 21, 23, 24)  # BBOB function numbers
