7dfcbf6326e50c06fc3b9a6d3be9fbbf11c4700b
Sander <svr003@gmail.com>
2016-01-11 17:33:15 +0100

    Added function to create convergence plots for GA

--- process_results.py
+++ process_results.py
@@ -4,11 +4,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 __author__ = 'Sander van Rijn <svr003@gmail.com>'
 
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 from code import getPrintName, getOpts
 
-
+np.set_printoptions(linewidth=156)
 location = 'D:\\test_results'  # laptop
 dims = [2, 3, 5, 10, 20]
 functions = [3, 4, 7, 9, 10, 12, 13, 16, 17, 19, 20, 21, 23, 24]
@@ -53,13 +54,43 @@ def printResults():
     for dim in dims:
         print("{}-dimensional:".format(dim))
         for func in functions:
-            # print(results[dim][func])
             print("  F{}:\t{} {}".format(func, results[dim][func]['best_result'], getPrintName(getOpts(results[dim][func]['best_result']))))
+
+
+def createGARunPlots():
+    os.chdir(location)
+    x = np.load('final_GA_results.npz')
+    results = x['results'].item()
+
+    for dim in dims:
+        print("{}-dimensional:".format(dim))
+        for func in functions:
+            best_per_generation = results[dim][func]['best_fitness'][::12]
+            best_found_ever = []
+            for i, fit in enumerate(best_per_generation):
+                if fit <= min(best_per_generation[:i+1]):
+                    best_found_ever.append(fit)
+                else:
+                    best_found_ever.append(best_found_ever[i-1])
+            # print(best_per_generation)
+            # print(best_found_ever)
+
+            plt.title("F{}, {}-dim".format(func, dim))
+            plt.plot(best_found_ever, label='Best overall')
+            plt.plot(best_per_generation, label='Best per generation')
+            plt.xlabel('Generation')
+            plt.ylabel('Fitness')
+            plt.legend(loc=0)
+
+            plt.savefig('F{}-{}dim.png'.format(func, dim))
+            plt.savefig('F{}-{}dim.pdf'.format(func, dim))
 
 if __name__ == '__main__':
 
     # storeResults()
     # printResults()
 
+    createGARunPlots()
 
     pass
+
