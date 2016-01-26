b994d7cb8e4c9e6be419a885cc0bf768bdcdedd5
Sander <svr003@gmail.com>
2016-01-26 16:53:50 +0100

    Big addition to post-processing for brute force results

--- process_results.py
+++ process_results.py
@@ -10,18 +10,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pprint
+import cPickle
 from code import getPrintName, getOpts
 
 np.set_printoptions(linewidth=156)
-# location = 'D:\\test_results'  # laptop
-location = '/home/sander/Dropbox/Liacs/Semester12/Thesis/test_results'  # desktop
+brute_location = 'C:\\Users\\Sander\\Dropbox\\Liacs\\Semester12\\Thesis\\brute_force\\final_results'
+
+ga_location = 'D:\\test_results'  # laptop
+# ga_location = '/home/sander/Dropbox/Liacs/Semester12/Thesis/test_results'  # desktop
 dims = [2, 3, 5, 10, 20]
 functions = [3, 4, 7, 9, 10, 12, 13, 16, 17, 19, 20, 21, 23, 24]
 np_save_names = ['time_spent', 'generation_sizes', 'sigma', 'best_result', 'best_fitness']
 
 
 def getBestEs():
-    os.chdir(location)
+    os.chdir(ga_location)
 
     results = {dim: {} for dim in dims}
 
@@ -52,7 +55,7 @@ def storeResults():
 
 def printResults():
 
-    os.chdir(location)
+    os.chdir(ga_location)
     x = np.load('final_GA_results.npz')
     results = x['results'].item()
     for dim in dims:
@@ -63,7 +66,7 @@ def printResults():
 
 def storeRepresentation():
 
-    os.chdir(location)
+    os.chdir(ga_location)
     x = np.load('final_GA_results.npz')
     results = x['results'].item()
 
@@ -79,24 +82,71 @@ def storeRepresentation():
         json.dump(to_store, json_out)
 
 
-
-def printTable():
-
-    os.chdir(location)
-    x = np.load('final_GA_results.npz')
-    results = x['results'].item()
+def printTable(results):
     print('\\hline')
-    print('F-ID Dim & & Best ES Found\\\\')
+    print('F-ID & Dim & Best ES Found\\\\')
     print('\\hline')
     print('\\hline')
-    for func in functions:
+    for fid in functions:
         for dim in dims:
-            print('F{} {} & & {}\\\\'.format(dim, func, getPrintName(getOpts(results[dim][func]['best_result']))))
+            print('F{} & {} & {}\\\\'.format(fid, dim, getPrintName(getOpts(results[dim][fid]))))
         print('\\hline')
 
 
+def printCompTable(bf, ga):
+    print('\\hline')
+    print('F-ID & Dim & Best ES & Best ES Found\\\\')
+    print('\\hline')
+    print('\\hline')
+    for fid in functions:
+        for dim in dims:
+            bf_result = bf[dim][fid]
+            ga_result = ga[dim][fid]
+            ga_diff = ''
+            bf_string = ''
+            for i in range(len(bf_result)):
+                bf_string += str(bf_result[i])
+                if bf_result[i] != ga_result[i]:
+                    ga_diff += '\\underline{}{}{}'.format('{', ga_result[i], '}')
+                else:
+                    ga_diff += str(ga_result[i])
+
+            print('F{} & {} & {} & {}\\\\'.format(fid, dim, bf_string, ga_diff))
+        print('\\hline')
+
+
+def printGATable():
+
+    os.chdir(ga_location)
+    x = np.load('final_GA_results.npz')
+    results = x['results'].item()
+    ga_results = {dim: {fid: results[dim][fid]['best_result'] for fid in functions} for dim in dims}
+    printTable(ga_results)
+
+
+def printBFTable():
+
+    os.chdir(brute_location)
+    with open('brute_results.dat') as f:
+        x = cPickle.load(f)
+        printTable(x)
+
+
+def printDoubleTable():
+
+    os.chdir(ga_location)
+    x = np.load('final_GA_results.npz')
+    results = x['results'].item()
+    ga_results = {dim: {fid: results[dim][fid]['best_result'] for fid in functions} for dim in dims}
+
+    os.chdir(brute_location)
+    with open('brute_results.dat') as f:
+        bf_results = cPickle.load(f)
+    printCompTable(bf_results, ga_results)
+
+
 def createGARunPlots():
-    os.chdir(location)
+    os.chdir(ga_location)
     x = np.load('final_GA_results.npz')
     results = x['results'].item()
 
@@ -125,8 +175,34 @@ def createGARunPlots():
         plt.savefig('img/F{}.png'.format(func), bbox_inches='tight')
         plt.savefig('img/F{}.pdf'.format(func), bbox_inches='tight')
 
+
+def findBestFromBF():
+    os.chdir(brute_location)
+
+    raw_fname = 'bruteforce_{}_f{}.tdat'
+    results = {dim: {} for dim in dims}
+
+    for dim in dims:
+        for fid in functions:
+            best_es = None
+            best_result = np.inf
+            with open(raw_fname.format(dim, fid)) as f:
+                for line in f:
+                    parts = line.split('\t')
+                    ES = eval(parts[0])
+                    fitness = np.median(eval(parts[1]))
+                    if fitness < best_result:
+                        best_result = fitness
+                        best_es = ES
+            results[dim][fid] = best_es
+
+    with open('brute_results.dat', 'w') as f:
+        cPickle.dump(results, f)
+
+
 if __name__ == '__main__':
 
+    ### GA STUFF ###
     # storeResults()
     # printResults()
 
@@ -139,6 +215,11 @@ if __name__ == '__main__':
     # with open('ES_per_experiment.json') as infile:
     #     x = json.load(infile)
     # pprint.pprint(x)
+
+    ### Brute Force STUFF ###
+
+    # findBestFromBF()
+    printDoubleTable()
 
     pass
 
