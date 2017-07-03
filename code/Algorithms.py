0b8dc013ebe2b40dabccd50eda2fca279d0ce9d1
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2017-07-03 17:10:02 +0200

    Removed a number of commented code sections; minimal/initial function documentation update of GA and MIES

--- code/Algorithms.py
+++ code/Algorithms.py
@@ -179,7 +179,7 @@ def CMA_ES(n, fitnessFunction, budget, mu=None, lambda_=None, elitist=False):
     parameters = Parameters(n, budget, mu, lambda_, elitist=elitist)
     population = [FloatIndividual(n) for _ in range(mu)]
 
-    # Artificial init: in hopes of fixing CMA-ES
+    # Artificial init
     wcm = parameters.wcm
     for individual in population:
         individual.genotype = wcm
@@ -797,26 +797,24 @@ def GA(n, fitnessFunction, budget):
     """
         Defines a Genetic Algorithm (GA) that evolves an Evolution Strategy (ES) for a given fitness function
 
-        :param n:       What dimensionality should the ES be evaluated in?
-        :param fid:     Which BBOB function should the ES be evaluated on?
-        :param budget:  The budget for the GA (N.B., this is *not* the underlying ES-budget)
-        :returns:       A tuple containing a bunch of optimization results
+        :param n:               Dimensionality of the search-space for the GA
+        :param fitnessFunction: Fitness function the GA should use to evaluate candidate solutions
+        :param budget:          The budget for the GA
+        :returns:               A tuple containing a bunch of optimization results
     """
 
-    # Assuming a dimensionality of 11 (8 boolean + 3 triples)
     GA_mu = Config.GA_mu
     GA_lambda = Config.GA_lambda
 
     parameters = Parameters(len(options) + 15, budget, mu=GA_mu, lambda_=GA_lambda)
     parameters.l_bound[len(options):] = np.array([  2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]).reshape(15,1)
     parameters.u_bound[len(options):] = np.array([200, 1, 5, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5]).reshape(15,1)
+
     # Initialize the first individual in the population
     population = [MixedIntIndividual(n, num_discrete=len(num_options), num_ints=1)]
     int_part = [np.random.randint(len(x[1])) for x in options]
     int_part.append(None)
-    # TODO FIXME: dumb, brute force, hardcoded defaults for testing purposes
     float_part = [None, None, None, None, None, None, None, None, None, None, None, None, None, None]
-    # float_part = [None, 2,    None, None, None, None, None, 0.2, 0.995, 0.5,  0,    0.3,  0.5,  2]
 
     population[0].genotype = np.array(int_part + float_part)
     population[0].fitness = ESFitness()
@@ -830,7 +828,7 @@ def GA(n, fitnessFunction, budget):
     best = Sel.bestGA
     def select(pop, new_pop, _, params):
         return best(pop, new_pop, params)
-    def mutateParameters(t):
+    def mutateParameters(_):
         pass  # The only actual parameter mutation is the self-adaptive step-size of each individual
 
     functions = {
@@ -847,15 +845,14 @@ def GA(n, fitnessFunction, budget):
 
 def MIES(n, fitnessFunction, budget):
     """
-        Defines a Genetic Algorithm (GA) that evolves an Evolution Strategy (ES) for a given fitness function
+        Defines a Mixed-Integer Evolution Strategy (MIES) that evolves an Evolution Strategy (ES) for a given fitness function
 
-        :param ndim:    What dimensionality should the ES be evaluated in?
-        :param fid:     Which BBOB function should the ES be evaluated on?
-        :param budget:  The budget for the GA (N.B., this is *not* the underlying ES-budget)
-        :returns:       A tuple containing a bunch of optimization results
+        :param n:               Dimensionality of the search-space for the MIES
+        :param fitnessFunction: Fitness function the MIES should use to evaluate candidate solutions
+        :param budget:          The budget for the MIES
+        :returns:               A tuple containing a bunch of optimization results
     """
 
-    # Assuming a dimensionality of 11 (8 boolean + 3 triples)
     GA_mu = Config.GA_mu
     GA_lambda = Config.GA_lambda
 
@@ -866,12 +863,8 @@ def MIES(n, fitnessFunction, budget):
 
     # Initialize the first individual in the population
     discrete_part = [np.random.randint(len(x[1])) for x in options]
-    # discrete_part.append(None)  # This is instead of integer value 'lambda_'
-    # TODO FIXME: dumb, brute force, hardcoded defaults for testing purposes
-    # float_part = [None, None, None, None, None, None, None, None, None, None, None, None, None, None]
     lamb = int(4 + floor(3 * log(parameters.n)))
     int_part = [lamb]
-    # float_part = [None, 2, 0.2, 0.995, 0.5, 0, 0.3,  0.5,  2]'damps', 'c_c', 'c_1', 'c_mu
     float_part = [parameters.mu, parameters.c_sigma, parameters.damps, parameters.c_c, parameters.c_1, parameters.c_mu,
                   None, 0.2, 0.955, 0.5, 0, 0.3, 0.5, 2]
     create_bounds(float_part, 0.3, parameters, options)
@@ -880,8 +873,6 @@ def MIES(n, fitnessFunction, budget):
                            num_ints=len(int_part))]
     population[0].genotype = np.array(discrete_part + int_part + float_part)
     population[0].fitness = ESFitness()
-    # population[0].genotype_temp = [None, None, None, None, None, None, None, None, None, None, None, None, None]
-    # print("num floats",population[0].num_floats)
 
     while len(population) < GA_mu:
         population.append(copy(population[0]))
@@ -889,13 +880,12 @@ def MIES(n, fitnessFunction, budget):
     # We use functions here to 'hide' the additional passing of parameters that are algorithm specific
     recombine = Rec.MIES_recombine
     mutate = partial(Mut.MIES_Mutate, options=options, num_options=num_options)
-    # mutate = Mut.MIES_Mutate(population[0], parameters,  options, num_options)
     best = Sel.bestGA
 
     def select(pop, new_pop, _, params):
         return best(pop, new_pop, params)
 
-    def mutateParameters(t):
+    def mutateParameters(_):
         pass  # The only actual parameter mutation is the self-adaptive step-size of each individual
 
     functions = {
