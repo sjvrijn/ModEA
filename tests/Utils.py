4044e9c89ee7d49f4c1731ba5734a5281c5595a1
Sander van Rijn <svr003@gmail.com>
2017-08-20 13:13:53 +0200

    Added tests for chunkListByLength, guaranteedFolder and ESFitness

--- tests/Utils.py
+++ tests/Utils.py
@@ -7,6 +7,7 @@ from collections import namedtuple
 from code.Utils import getFitness, reprToString, reprToInt, intToRepr, \
     create_bounds, chunkListByLength, guaranteeFolderExists, ESFitness
 import numpy as np
+import os
 
 
 class MockIndividual(object):
@@ -61,6 +62,116 @@ class BoundsTest(unittest.TestCase):
     def test_incorrect_percentage(self):
         with self.assertRaises(ValueError):
             create_bounds(self.values, percentage=3.0)
+
+
+class ChunkListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.values = list(range(10))
+
+    def test_chunk_empty_list(self):
+        self.assertListEqual(list(chunkListByLength([], 3)),
+                             [])
+
+    def test_chunk_square_list(self):
+        self.assertListEqual(list(chunkListByLength(range(6), 3)),
+                             [[0, 1, 2], [3, 4, 5]])
+
+    def test_chunk_ragged_list(self):
+        self.assertListEqual(list(chunkListByLength(range(7), 3)),
+                             [[0, 1, 2], [3, 4, 5], [6]])
+
+
+class GuaranteedFolderTest(unittest.TestCase):
+
+    def setUp(self):
+        self.folder_name = 'test_folder_abc_xyz'
+
+    def tearDown(self):
+        os.rmdir(self.folder_name)
+
+    def test_create_folder(self):
+        guaranteeFolderExists(self.folder_name)
+        self.assertIn(self.folder_name, os.listdir('.'))
+
+    def test_use_existing_folder(self):
+        os.mkdir(self.folder_name)
+        guaranteeFolderExists(self.folder_name)
+        self.assertIn(self.folder_name, os.listdir('.'))
+
+
+class ESFitnessTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_from_human_radable_values(self):
+        es1 = ESFitness(ERT=None, FCE=float('inf'))
+        self.assertIs(es1.ERT, None)
+        self.assertEqual(es1.FCE, float('inf'))
+
+        es2 = ESFitness(ERT=None, FCE=float(32))
+        self.assertIs(es2.ERT, None)
+        self.assertEqual(es2.FCE, float(32))
+
+        es3 = ESFitness(ERT=2   , FCE=float('inf'))
+        self.assertEqual(es3.ERT, 2)
+        self.assertEqual(es3.FCE, float('inf'))
+
+        es4 = ESFitness(ERT=3   , FCE=float(42))
+        self.assertEqual(es4.ERT, 3)
+        self.assertEqual(es4.FCE, float(42))
+
+    def test_create_both_from_summary_values(self):
+        es = ESFitness(target=0,
+                       min_fitnesses=[ 0,  0,  40,  0,  0],
+                       min_indices=  [32, 48, 999, 16, 64],
+                       num_successful=4)
+        self.assertAlmostEqual(es.ERT, 289.75)
+        self.assertAlmostEqual(es.std_dev_ERT, 383.9335359147)
+        self.assertAlmostEqual(es.FCE, 8)
+        self.assertAlmostEqual(es.std_dev_FCE, 16)
+
+    def test_create_FCE_from_summary_values(self):
+        es = ESFitness(target=0,
+                       min_fitnesses=[  1,   2,  42,   3,   4],
+                       min_indices=  [999, 999, 999, 999, 999],
+                       num_successful=0)
+        self.assertIs(es.ERT, None)
+        self.assertAlmostEqual(es.std_dev_ERT, 0)
+        self.assertAlmostEqual(es.FCE, 10.4)
+        self.assertAlmostEqual(es.std_dev_FCE, 15.83161394173)
+
+    def test_create_from_original_values(self):
+        pass  # TODO
+
+    def test_correct_sorting(self):
+        es1 = ESFitness(ERT=None, FCE=float('inf'))
+        es2 = ESFitness(ERT=None, FCE=float(32))
+        es3 = ESFitness(ERT=2   , FCE=float('inf'))
+        es4 = ESFitness(ERT=3   , FCE=float(42))
+        self.assertListEqual(sorted([es1, es2, es3, es4]), [es3, es4, es2, es1])
+
+    def test_equality(self):
+        es1 = ESFitness(ERT=42, FCE=5)
+        es2 = ESFitness(ERT=42, FCE=42)
+        self.assertTrue(es1 == es2)
+
+        es1 = ESFitness(ERT=None, FCE=42)
+        es2 = ESFitness(ERT=None, FCE=42)
+        self.assertTrue(es1 == es2)
+
+    def test_inequality(self):
+        es1 = ESFitness(ERT=None, FCE=5)
+        es2 = ESFitness(ERT=None, FCE=42)
+        self.assertTrue(es1 != es2)
+
+        es1 = ESFitness(ERT=3, FCE=42)
+        es2 = ESFitness(ERT=4, FCE=42)
+        self.assertTrue(es1 != es2)
 
 
 if __name__ == '__main__':
