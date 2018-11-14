ecadc1043cb2b3547ae97c454d37e549ae35b064
Sander van Rijn <svr003@gmail.com>
2018-11-14 23:02:33 +0100

    Updated setup.py to match new package name

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name='modular-cmaes-framework',
-    version='0.0.0',
-    description='A modular evolutionary algorithm framework mostly tailored to a modular implementation of the CMA-ES',
+    name='ModEA',
+    version='0.3.0',
+    description='A modular evolutionary algorithm framework, mostly tailored to a modular implementation of the CMA-ES',
     long_description=long_description,
     author='Sander van Rijn',
     author_email='s.j.van.rijn@liacs.leidenuniv.nl',
-    packages=['modularcmaes'],
+    packages=['modea'],
 )
