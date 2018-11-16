e1ce1792d910f5c83f10dd4dee86f28c03a9bd19
Sander van Rijn <svr003@gmail.com>
2018-11-16 20:18:07 +0100

    updated version number in __init__ and added name attribute

--- modea/__init__.py
+++ modea/__init__.py
@@ -2,5 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+name = 'modea'
 __author__ = 'Sander van Rijn <svr003@gmail.com>'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
